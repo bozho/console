55a503ae36988234fa9a3db410a53a74cdebde12
Bozho <Bozho@8ae08d04-e83e-804b-908e-3db21359c104>
2006-07-22 20:52:52 +0000

    - fixed rows/columns display updating in the statusbar
    - added support for multiple startup tabs and dirs (-t, -d, -r and -ts cmd-line options)
    
    git-svn-id: svn://localhost/private/Console2/trunk@379 8ae08d04-e83e-804b-908e-3db21359c104

--- Console2/ConsoleHook/stdafx.h
+++ Console2/ConsoleHook/stdafx.h
@@ -49,7 +49,7 @@ using namespace boost;
 
 #define	VERSION_MAJOR	2
 #define	VERSION_MINOR	0
-#define	VERSION_BUILD	124
+#define	VERSION_BUILD	125
 
 //////////////////////////////////////////////////////////////////////////////
 

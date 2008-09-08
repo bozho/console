dc666de051bc821d5f6aabe21dd3022f374e7bf6
bozho <bozho@8ae08d04-e83e-804b-908e-3db21359c104>
2008-09-08 19:23:56 +0000

    - Console will now inherit environment block on startup (it used to get a fresh environment block in b140); it will still track changes in the environment, like  it was implemented in b140
    git-svn-id: svn://localhost/private/Console2/trunk@967 8ae08d04-e83e-804b-908e-3db21359c104

--- Console/stdafx.h
+++ Console/stdafx.h
@@ -107,7 +107,7 @@ using namespace boost::multi_index;
 
 #define	VERSION_MAJOR	2
 #define	VERSION_MINOR	0
-#define	VERSION_BUILD	140
+#define	VERSION_BUILD	141
 
 //////////////////////////////////////////////////////////////////////////////
 

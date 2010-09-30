18e1fe28acd86b4b72901ab257a271dfe848525c
Marko Bozikovic <bozho@kset.org>
2010-09-30 17:47:39 +0200

    Fixed invalid window position on Windows unlock

--- ConsoleHook/stdafx.h
+++ ConsoleHook/stdafx.h
@@ -5,7 +5,7 @@
 
 #pragma once
 
-#define _WIN32_WINNT 0x0500
+#define _WIN32_WINNT 0x0501
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
 //////////////////////////////////////////////////////////////////////////////

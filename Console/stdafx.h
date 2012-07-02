df261dc60ff27acb21a068a499414f9eca6f37a1
cbucher <christophe.bucher@laposte.net>
2012-07-02 09:56:49 +0200

    version 1.3.1

--- Console/stdafx.h
+++ Console/stdafx.h
@@ -139,7 +139,7 @@ using namespace boost::multi_index;
 
 #define	VERSION_MAJOR	1
 #define	VERSION_MINOR	3
-#define	VERSION_BUILD	0
+#define	VERSION_BUILD	1
 
 //////////////////////////////////////////////////////////////////////////////
 

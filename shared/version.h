e7af061ca019ea00762293cff213120cbdc43bb1
cbucher <christophe.bucher@laposte.net>
2012-11-25 08:59:40 +0100

    version 1.4.0

--- shared/version.h
+++ shared/version.h
@@ -3,7 +3,7 @@
 #define VERSION_MAJOR     1
 #define VERSION_MINOR     4
 #define VERSION_BUILD     0
-#define VERSION_BUILD2    12305
+#define VERSION_BUILD2    12329
 #define VERSION_FILE      "1.4.0"
-#define VERSION_PRODUCT   "1, 4, 0, 12305"
+#define VERSION_PRODUCT   "1, 4, 0, 12329"
 #define VERSION_COPYRIGHT "Copyright © 2011-2012 Bucher Christophe"

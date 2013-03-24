af0752b81ff8a04bc1a859a8e7a63dc15a5721de
cbucher <christophe.bucher@laposte.net>
2013-03-24 09:35:07 +0100

    version 1.6.0.13083

--- shared/version.h
+++ shared/version.h
@@ -1,9 +1,9 @@
-#pragma once
+ï»¿#pragma once
 
 #define VERSION_MAJOR     1
-#define VERSION_MINOR     5
+#define VERSION_MINOR     6
 #define VERSION_BUILD     0
-#define VERSION_BUILD2    13033
-#define VERSION_FILE      "1.5.0"
-#define VERSION_PRODUCT   "1, 5, 0, 13033"
-#define VERSION_COPYRIGHT "Copyright © 2011-2013 Bucher Christophe"
+#define VERSION_BUILD2    13083
+#define VERSION_FILE      "1.6.0"
+#define VERSION_PRODUCT   "1, 6, 0, 13083"
+#define VERSION_COPYRIGHT "Copyright (C) 2011-2013 Bucher Christophe"

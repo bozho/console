5b785dacbddf110c45339ed92bb3b5cb17a9171e
cbucher <christophe.bucher@laposte.net>
2013-11-03 10:00:28 +0100

    #49 Fix compilation with VS 2013

--- Console/ImageHandler.cpp
+++ Console/ImageHandler.cpp
@@ -218,7 +218,7 @@ bool ImageHandler::GetDesktopImageData(ImageData& imageData)
 	if (keyColors.QueryStringValue(L"Background", strBackground.GetBuffer(dwDataSize), &dwDataSize) != ERROR_SUCCESS)
 	{
 		strBackground.ReleaseBuffer();
-		return std::shared_ptr<BackgroundImage>();
+		return false;
 	}
 	strBackground.ReleaseBuffer();
 

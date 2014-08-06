e94d1970104b79dede45cec76c1faaee4cff3a12
cbucher <christophe.bucher@laposte.net>
2014-08-06 18:35:09 +0200

    #130 load/save environment variables per tabs missing files in previous commit

---
+++ Console/PageSettingsTabsEnv.cpp
@@ -0,0 +1,129 @@
+#include "stdafx.h"
+#include "resource.h"
+
+#include "PageSettingsTabsEnv.h"
+#include "XmlHelper.h"
+
+extern std::shared_ptr<SettingsHandler>	g_settingsHandler;
+
+PageSettingsTabsEnv::PageSettingsTabsEnv()
+: m_tabData()
+{
+}
+
+LRESULT PageSettingsTabsEnv::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+{
+	ExecuteDlgInit(IDD);
+
+	if (CTheme().IsThemingSupported()) ::EnableThemeDialogTexture(m_hWnd, ETDT_USETABTEXTURE);
+
+	m_listCtrl.Attach(GetDlgItem(IDC_LIST_ENV));
+	m_listCtrl.SetExtendedListViewStyle(m_listCtrl.GetExtendedListViewStyle() | LVS_EX_FULLROWSELECT | LVS_EX_CHECKBOXES);
+
+	m_listCtrl.InsertColumn(0, L"Variable");
+	m_listCtrl.InsertColumn(1, L"Value");
+
+	m_listCtrl.SetColumnWidth(0, 100);
+	m_listCtrl.SetColumnWidth(1, LVSCW_AUTOSIZE_USEHEADER);
+
+	m_editVariable.Attach(GetDlgItem(IDC_ENV_VARIABLE));
+	m_editValue.Attach(GetDlgItem(IDC_ENV_VALUE));
+
+	return TRUE;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+void PageSettingsTabsEnv::Load(shared_ptr<TabData>& tabData)
+{
+	m_tabData = tabData;
+
+	DoDataExchange(DDX_LOAD);
+
+	m_listCtrl.DeleteAllItems();
+	for(size_t i = 0; i < m_tabData->strEnvVariables.size(); ++i)
+	{
+		int nItem = m_listCtrl.InsertItem(m_listCtrl.GetItemCount(), m_tabData->strEnvVariables[i].c_str());
+		m_listCtrl.SetCheckState(nItem, m_tabData->bEnvChecked[i]);
+		m_listCtrl.SetItemText(nItem, 1, m_tabData->strEnvValues[i].c_str());
+	}
+}
+
+void PageSettingsTabsEnv::Save()
+{
+	DoDataExchange(DDX_SAVE);
+
+	m_tabData->strEnvVariables.clear();
+	m_tabData->strEnvValues.clear();
+	m_tabData->bEnvChecked.clear();
+
+	for(int nItem = 0; nItem < m_listCtrl.GetItemCount(); ++nItem)
+	{
+		CString strVariable;
+		m_listCtrl.GetItemText(nItem, 0, strVariable);
+		CString strValue;
+		m_listCtrl.GetItemText(nItem, 1, strValue);
+
+		m_tabData->strEnvVariables.push_back(strVariable.GetString());
+		m_tabData->strEnvValues.push_back(strValue.GetString());
+		m_tabData->bEnvChecked.push_back(m_listCtrl.GetCheckState(nItem)? true : false);
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+LRESULT PageSettingsTabsEnv::OnClickedBtnEnvAdd(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+	CString strVariable;
+	m_editVariable.GetWindowText(strVariable);
+
+	CString strValue;
+	m_editValue.GetWindowText(strValue);
+
+	LVFINDINFO findInfo;
+	findInfo.flags = LVFI_STRING;
+	findInfo.psz = strVariable.GetString();
+	int nItem = m_listCtrl.FindItem(&findInfo, -1);
+
+	if( nItem == -1 )
+	{
+		nItem = m_listCtrl.InsertItem(m_listCtrl.GetItemCount(), strVariable);
+		m_listCtrl.SetCheckState(nItem, TRUE);
+	}
+
+	m_listCtrl.SetItemText(nItem, 1, strValue);
+
+	return 0;
+}
+
+LRESULT PageSettingsTabsEnv::OnClickedBtnEnvDelete(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+	int nItem = m_listCtrl.GetSelectedIndex();
+	if( nItem < 0 || nItem >= m_listCtrl.GetItemCount() ) return 0;
+
+	m_listCtrl.DeleteItem(nItem);
+
+	return 0;
+}
+
+LRESULT PageSettingsTabsEnv::OnClickedBtnEnvEdit(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+	int nItem = m_listCtrl.GetSelectedIndex();
+	if( nItem < 0 || nItem >= m_listCtrl.GetItemCount() ) return 0;
+	
+	CString strVariable;
+	m_listCtrl.GetItemText(nItem, 0, strVariable);
+	CString strValue;
+	m_listCtrl.GetItemText(nItem, 1, strValue);
+
+	m_editVariable.SetWindowText(strVariable);
+	m_editValue.SetWindowText(strValue);
+
+	return 0;
+}
---
+++ Console/PageSettingsTabsEnv.h
@@ -0,0 +1,47 @@
+
+#pragma once
+
+#include "PageSettingsTab.h"
+
+class PageSettingsTabsEnv
+	: public CDialogImpl<PageSettingsTabsEnv>
+	, public CWinDataExchange<PageSettingsTabsEnv>
+	, public PageSettingsTab
+{
+	public:
+
+		enum { IDD = IDD_SETTINGS_TABS_ENV };
+
+		PageSettingsTabsEnv();
+
+		BEGIN_DDX_MAP(PageSettingsTabsEnv)
+		END_DDX_MAP()
+
+		BEGIN_MSG_MAP(PageSettingsTabsEnv)
+			MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+
+			COMMAND_ID_HANDLER(IDC_BTN_ENV_ADD,    OnClickedBtnEnvAdd)
+			COMMAND_ID_HANDLER(IDC_BTN_ENV_DELETE, OnClickedBtnEnvDelete)
+			COMMAND_ID_HANDLER(IDC_BTN_ENV_EDIT,   OnClickedBtnEnvEdit)
+		END_MSG_MAP()
+
+		LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
+
+		LRESULT OnClickedBtnEnvAdd   (WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+		LRESULT OnClickedBtnEnvDelete(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+		LRESULT OnClickedBtnEnvEdit  (WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+
+		void Load(shared_ptr<TabData>& tabData);
+		void Save();
+
+	private:
+
+		shared_ptr<TabData> m_tabData;
+
+		CListViewCtrl       m_listCtrl;
+		CEdit               m_editVariable;
+		CEdit               m_editValue;
+
+};
+
+//////////////////////////////////////////////////////////////////////////////

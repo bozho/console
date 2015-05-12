b02dc1fc60f5804274edf7124e7ad7476ba3dd60
cbucher <christophe.bucher@laposte.net>
2015-05-12 07:47:17 +0200

    version 1.14.0.15129

--- shared/version.h
+++ shared/version.h
@@ -3,13 +3,13 @@
 #define VERSION_MAJOR     1
 #define VERSION_MINOR     14
 #define VERSION_BUILD     0
-#define VERSION_BUILD2    15128
+#define VERSION_BUILD2    15129
 #define VERSION_FILE      "1.14.0"
-#define VERSION_PRODUCT   "1, 14, 0, 15128"
+#define VERSION_PRODUCT   "1, 14, 0, 15129"
 #define VERSION_COPYRIGHT "Copyright (C) 2011-2015 Bucher Christophe"
 
 #ifndef _WIN32
 
-VERSION = 1.14.0.15128
+VERSION = 1.14.0.15129
 
 #endif
